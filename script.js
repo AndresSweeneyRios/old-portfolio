4014c868b780bea1d5fcc7a83ca42e0b31b323c1
Andr3wRiv3rs <andrewarivers@gmail.com>
2018-12-11 13:13:49 -0800

    Render & Melt

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ const page = new Vue({
 
 fetch (location.origin + '/entries.json')
     .then(d=>d.json())
-    .then(d=>page.entries=d.entries)
-    .catch(console.error);
+    .then(d=>{
+        page.entries = d.entries;
+        renderHTML();
+    }).catch(console.error);
 
 window.addEventListener('click', e => {
     switch (e.target.id) {
@@ -20,3 +22,31 @@ window.addEventListener('click', e => {
         case 'contact': page.page = 'contact'; break;
     }
 });
+
+const renderHTML = () => {
+    if ($('#html-canvas')) {
+        $('#html-canvas').getContext('2d').clearRect(0,0,$('#html-canvas').width,$('#html-canvas').height);
+        $('#html-canvas').remove();
+    }
+
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        html2canvas(document.body).then(canvas => {
+            canvas.id = "html-canvas";
+            document.body.prepend(canvas);
+            meltCanvas();
+        });
+    }
+
+}
+
+const meltCanvas = (
+    scale = 0
+) => {
+    setInterval(()=>{
+        scale+=1; 
+        $('#displacementFilter').children[1].setAttribute('scale',scale)
+    },50);
+};
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) 
+    $('body').style.backgroundImage = 'url(static_bg.png)';
