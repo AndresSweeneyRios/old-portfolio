3ef839ea4e7b1695ed65c52fb42f580cec09f96d
Andr3wRiv3rs <andrewarivers@gmail.com>
2018-12-26 17:23:57 -0800

    Added Entries

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ const page = new Vue({
     el: '#page',
     data: {
         entries: [],
-        page: 'home'
+        page: 'home',
+        bio: '',
+        skillset: []
     }
 });
 
-fetch (location.origin + '/entries.json')
-    .then(d=>d.json())
-    .then(d=>{
-        page.entries = d.entries;
-        if (html2canvas) page.$nextTick(renderHTML);
-    }).catch(console.error);
+const get = w => fetch (location.origin + '/' + w).then(d=>d.text())
+const getJSON = w => fetch (location.origin + '/' + w).then(d=>d.json())
+
+getJSON ('entries.json')
+    .then(d=>page.entries = d.entries)
+    .catch(console.error);
+
+get ('bio.html')
+    .then(d=>page.bio = d)
+    
 
 window.addEventListener('click', e => {
     switch (e.target.id) {
@@ -24,19 +30,13 @@ window.addEventListener('click', e => {
 });
 
 const renderHTML = () => {
-    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        if ($('#html-canvas')) {
-            $('#html-canvas').getContext('2d').clearRect(0,0,$('#html-canvas').width,$('#html-canvas').height);
-            $('#html-canvas').remove();
-        }
-
-        html2canvas(document.body).then(canvas => {
-            canvas.id = "html-canvas";
-            document.body.prepend(canvas);
-        });
+    if ($('#html-canvas')) {
+        $('#html-canvas').getContext('2d').clearRect(0,0,$('#html-canvas').width,$('#html-canvas').height);
+        $('#html-canvas').remove();
     }
 
+    html2canvas(document.body).then(canvas => {
+        canvas.id = "html-canvas";
+        document.body.prepend(canvas);
+    });
 }
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) 
-    $('body').style.backgroundImage = 'url(static_bg.png)';
